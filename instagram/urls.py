35d0800333b67432c33b5b1f282317b13ac256ce
brwonbreeze <913breeze@gmail.com>
2023-05-27 16:20:07 +0000

    json 응답 뷰 만들기
    - django 내 PostSerializer 을 통해 views개발 속도 높임

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,3 +1,11 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register('post', views.PostViewSet) # 두개의 URL 을 생성 
+# router.urls # pattern list 
+
 urlpatterns = [
-    
+    path('', include(router.urls)),
 ]
--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializers import PostSerializer
+from .models import Post
 
-# Create your views here.
+# 아래 두개의 url을 해당 값으로 대체 가능 
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer    
+
+# def post_list(request):
+#     #최소 두개 분리 
+#     pass
+# def post_detail(request, pk):
+#     #최소 세개 분기 
+#     pass
