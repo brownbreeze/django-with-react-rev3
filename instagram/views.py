c0c2a6c06af78f19e213b7355aac7e2d7c49cabc
brwonbreeze <913breeze@gmail.com>
2023-06-09 10:11:58 +0000

    APIView를 활용한 뷰 만들기
    - clean code
    - 2) function public_post_list 반영 상태

--- instagram/views.py
+++ instagram/views.py
@@ -1,17 +1,16 @@
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .serializers import PostSerializer
-from rest_framework.views import APIView
 from .models import Post
 
-class PublicPostListAPIView(APIView):
-    def get(self, request):
-        qs = Post.objects.filter(is_public=True)
-        serializer = PostSerializer(qs, many=True)
-        return Response(serializer.data)
-
-public_post_list = PublicPostListAPIView.as_view()
+@api_view(['GET'])
+def public_post_list(request):
+    qs = Post.objects.filter(is_public=True)
+    serializer = PostSerializer(qs, many=True)
+    return Response(serializer.data)
 
 # 아래 두개의 url을 해당 값으로 대체 가능 
 class PostViewSet(ModelViewSet):
